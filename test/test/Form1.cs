59dfcacc5382cfd824a5b06e11a88c5910ca8013
ViktorKartalov <vkartalcho@gmail.com>
2022-03-10 19:48:12 +0200

    Added test app viktor

---
+++ test/test/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private string text;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            text = textBoxText.Text;
+            buttonSave.Text = text;
+        }
+    }
+}
